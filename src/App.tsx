848c3346306da95d263c03ebd429a282e1534ba0
bruno <brunoqueiroz612@gmail.com>
2023-06-22 08:29:01 -0300

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,41 @@
+import React from "react";
+
+const baseUrl = "http://localhost:3000";
+
+const addCookie = async () => {
+  const response = await fetch(`${baseUrl}/add-cookie`, {
+    credentials: "include",
+  });
+
+  const data = await response.json();
+  return data;
+};
+
+const removeCookie = async () => {
+  const response = await fetch(`${baseUrl}/remove-cookie`, {
+    credentials: "include",
+  });
+
+  const data = await response.json();
+  return data;
+};
+
+const App = () => {
+  const click = async () => {
+    const data = await addCookie();
+    console.log(data);
+  };
+
+  const remove = async () => {
+    const data = await removeCookie();
+    console.log(data);
+  };
+  return (
+    <div style={{ padding: "2rem" }}>
+      <button onClick={click}>Add cookie</button>
+      <button onClick={remove}>remove cookies</button>
+    </div>
+  );
+};
+
+export default App;
